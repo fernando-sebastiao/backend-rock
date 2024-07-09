7522f2a46c90b993417b2829659d7c9456131bf4
= <=>
2024-07-09 15:23:58 +0100

    feito o envio de mensagem com adaptação a código html

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -18,34 +18,53 @@ export async function createTrip(app: FastifyInstance) {
           ends_at: z.coerce.date(),
           owner_name: z.string(),
           owner_email: z.string().email({ message: "Invalid email" }),
+          emails_to_invite: z.array(
+            z.string().email({ message: "Invalid email" })
+          ),
         }),
       },
     },
     async (request) => {
-      const { destination, starts_at, ends_at, owner_name, owner_email } =
-        request.body as {
-          destination: string;
-          starts_at: Date;
-          ends_at: Date;
-          owner_name: string;
-          owner_email: string;
-        };
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_name,
+        owner_email,
+        emails_to_invite,
+      } = request.body as {
+        destination: string;
+        starts_at: Date;
+        ends_at: Date;
+        owner_name: string;
+        owner_email: string;
+        emails_to_invite: string[];
+      };
       if (dayjs(starts_at).isBefore(new Date())) {
         throw new Error("Invalid trip start date");
       }
       if (dayjs(ends_at).isBefore(dayjs(starts_at))) {
         throw new Error("Invalid trip end date");
       }
-
+      //criando a trip e o participante
       const trip = await prisma.trip.create({
         data: {
           destination,
           starts_at,
           ends_at,
           pacticipants: {
-            create: {
-              name: owner_name,
-              email: owner_email,
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+                ...emails_to_invite.map((email) => {
+                  return { email };
+                }),
+              ],
             },
           },
         },
@@ -62,8 +81,18 @@ export async function createTrip(app: FastifyInstance) {
           name: owner_name,
           address: owner_email,
         },
-        subject: "testando envio de e-mail",
-        html: "<p>Teste de envio de e-mail</p>",
+        subject: "Confirme sua viagem para  ${destination}",
+        html: `
+<div style="font-family: sans-serif; font-size: 16px;line-height: 1.6;">
+<p></p>
+<p> Você solicitou a criação de uma recolha para  ${destination} <strong>nas datas</strong> 16 a 27 de Agosto </p>
+<p>para confirmar a sua viagem clique no link à baixo</p>
+<p>
+<a href="">Confirmar viagens</a>
+</p>
+<p>Caso você não siaba do que se trata este e-mail, apenas ignore o e-mail</p>
+</div>
+`.trim(),
       });
       console.log(nodemailer.getTestMessageUrl(message));
       return { trip: trip.id };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
+import cors from "@fastify/cors";
 import fastify from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createTrip } from "./routes/create-trip";
-
 const app = fastify();
+app.register(cors, {});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
