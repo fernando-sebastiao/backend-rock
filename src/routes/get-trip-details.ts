333f70c404dd94d61b5bea0c0924672e3d0e79f1
fernando-sebastiao <fernandosebastiao888@gmail.com>
2024-07-10 22:22:16 +0100

    fazendo o get-trip-details

---
+++ src/routes/get-trip-details.ts
@@ -0,0 +1,41 @@
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+import localizedFormat from "dayjs/plugin/localizedFormat";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+
+import { prisma } from "../lib/prisma";
+
+dayjs.locale("pt-br");
+
+dayjs.extend(localizedFormat);
+
+export async function getTripsDetails(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/trips/:tripId/links",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+      },
+    },
+    async (request) => {
+      const { tripId } = request.params;
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+        include: {
+          links: true,
+        },
+      });
+      if (!trip) {
+        throw new Error("Trip not found");
+      }
+      return { links: trip.links };
+    }
+  );
+}
